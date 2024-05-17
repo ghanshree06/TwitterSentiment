42ae725888c5d0ee1511e8c8ba2266e6b2a53d54
ghanshree06 <104723118+ghanshree06@users.noreply.github.com>
2024-05-17 15:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ search_words = st.text_input('Enter the name for which you want to know the sent
 number_of_tweets = st.number_input('Enter the number of latest tweets for which you want to know the sentiment(Maximum 50 tweets)', 0,50,10)
 submit_button = st.form_submit_button(label='Submit')
 if submit_button:
-tweets =tw.Cursor(api.search_tweets,q=search_words,lang=”en”).items(number_of_tweets)
+tweets =tw.Cursor(api.search_tweets,q=search_words,lang="en").items(number_of_tweets)
 tweet_list = [i.text for i in tweets]
 p = [i for i in classifier(tweet_list)]
 q=[p[i][‘label’] for i in range(len(p))]
-df = pd.DataFrame(list(zip(tweet_list, q)),columns =[‘Latest ‘+str(number_of_tweets)+’ Tweets’+’ on ‘+search_words, ‘sentiment’])
+df = pd.DataFrame(list(zip(tweet_list, q)),columns =['Latest '+str(number_of_tweets)+'Tweets'+'' on '+search_words, 'sentiment'])
 st.write(df)
  
 
-if __name__==’__main__’:
+if __name__=='__main__':
 run()
