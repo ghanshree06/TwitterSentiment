82f08787d62c0e0dd9a82d00f0c076f51afe0551
ghanshree06 <104723118+ghanshree06@users.noreply.github.com>
2024-05-17 15:40:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ api = tw.API(auth, wait_on_rate_limit=True)
 st.title('Live Twitter Sentiment Analysis with Tweepy and HuggingFace Transformers')
 st.markdown('This app uses tweepy to get tweets from twitter based on the input name/phrase. It then processes the tweets through HuggingFace transformers pipeline function for sentiment analysis. The resulting sentiments and corresponding tweets are then put in a dataframe for display which is what you see as result.')
 def run():
-with st.form(key='Enter name'):
+with st.form(key='ghanshree'):
 search_words = st.text_input('Enter the name for which you want to know the sentiment')
 number_of_tweets = st.number_input('Enter the number of latest tweets for which you want to know the sentiment(Maximum 50 tweets)', 0,50,10)
 submit_button = st.form_submit_button(label='Submit')
